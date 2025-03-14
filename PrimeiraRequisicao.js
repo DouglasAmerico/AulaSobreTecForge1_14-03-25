0ec49bc035e88ea7a8e5ff0cb0e9063808490919
Douglas Américo <douglas@likesistemas.com.br>
2025-03-14 20:35:37 -0300

    inserindo a primeira requisição utilizando o prompt-sync

---
+++ PrimeiraRequisicao.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Por favor informe seu nome: ");
+
+console.log("O nome digitado é: "+nome);
--- index.js
+++
@@ -1,7 +0,0 @@
-console.log("------");
-
-
-console.log("Hello Word");
-
-
-console.log("Essa é a melhor turma");
